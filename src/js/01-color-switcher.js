42d57bb76086d675e3846fc1e24eddf086e49050
tmitsay <132346077+tmitsay@users.noreply.github.com>
2023-09-28 17:57:05 +0300

    done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  body: document.body,
+  bntElStart: document.querySelector('button[data-start]'),
+  btnElStop: document.querySelector('button[data-stop]'),
+};
 
+refs.bntElStart.addEventListener('click', onStart);
+refs.btnElStop.addEventListener('click', onStop);
+let intervalId = null;
+
+function onStart() {
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.bntElStart.setAttribute('disabled', true);
+  refs.btnElStop.removeAttribute('disabled');
+}
+
+function onStop() {
+  clearInterval(intervalId);
+  refs.bntElStart.removeAttribute('disabled');
+  refs.btnElStop.setAttribute('disabled', true);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
